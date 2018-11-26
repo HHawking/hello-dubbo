7ab6017bb6fd2588b792bc9e564d48df4d5fcfd7
HHawking <hhawking@aliyun.com>
2018-11-26 21:17:10 +0800

    创建项目

---
+++ hello-dubbo-service-user-provider/src/main/java/com/hhawking/hello/dubbo/service/user/provider/HelloDubboServiceUserProviderApplication.java
@@ -0,0 +1,17 @@
+package com.hhawking.hello.dubbo.service.user.provider;
+
+import org.springframework.boot.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDubboServiceUserProviderApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloDubboServiceUserProviderApplication.class, args);
+    }
+
+}
